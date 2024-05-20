76ac86d53234333a8d17b35f21f87dc3f174ecf1
unknown <carolinehoeg123@gmail.com>
2024-05-20 15:11:56 +0200

    small

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,18 @@ use clap::Parser;
 use std::env;
 use exam_project::{
     cli::{Cli, CommandExecutor as _},
-    users_db::{self, UserManager},
+    users_db::UserManager,
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli: Cli = Cli::parse();
-    let (conn_string, users_db, users_collection) = get_env_values_or_use_default();
-    let um: UserManager = UserManager::new(&&conn_string, &&users_db, &&users_collection)?;
+    let (conn_string, users_db, users_collection) = get_env_values();
+    let um: UserManager = UserManager::new(&conn_string, &users_db, &users_collection)?;
     cli.command.execute(um)?;
     Ok(())
 }
 
-fn get_env_values_or_use_default() -> (String, String, String) {
+fn get_env_values() -> (String, String, String) {
     // Retrieve MongoDB connection details from environment variables
     let conn_string = env::var("MONGODB_URL").expect("Missing environment variable MONGODB_URL");
     let users_db = env::var("MONGODB_DATABASE").expect("Missing environment variable MONGODB_DATABASE");
