b80f16fbcbbd124c5a8e5fc1437d5f744d27a6a8
Maria McNally <70536109+cph-mm620@users.noreply.github.com>
2024-05-20 14:52:59 +0200

    add the env values

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::env;
 use exam_project::{
     cli::{Cli, CommandExecutor as _},
     users_db::UserManager,
@@ -6,11 +7,17 @@ use exam_project::{
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli: Cli = Cli::parse();
-    let um: UserManager = UserManager::new("mongodb://localhost:27017", "rust", "users")?;
+    let (conn_string, users_db, users_collection) = get_env_values_or_use_default();
+    let um: UserManager = UserManager::new(&conn_string, &users_db, &users_collection)?;
     cli.command.execute(um)?;
     Ok(())
 }
 
-fn get_env_values_or_use_default() {
-    // return env variables
+fn get_env_values_or_use_default() -> (String, String, String) {
+    // Retrieve MongoDB connection details from environment variables
+    let conn_string = env::var("MONGODB_URL").unwrap_or_else(|_| "mongodb://localhost:27017".to_string());
+    let users_db = env::var("MONGODB_DATABASE").unwrap_or_else(|_| "rust".to_string());
+    let users_collection = env::var("MONGODB_COLLECTION").unwrap_or_else(|_| "users".to_string());
+
+    (conn_string, users_db, users_collection)
 }
